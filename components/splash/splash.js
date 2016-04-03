701ab6b5028fdc98e23e65ed02316dcfa2d770e3
Kirill Kaysarov <defcuts@mail.ru>
2016-04-03 20:39:20 +0600

    Fix splash show up

--- components/splash/splash.js
+++ components/splash/splash.js
@@ -1,10 +1,20 @@
 var css = require('./splash.css');
-var extend = require('catbee-utils').extend;
+var { cut, extend } = require('catbee-utils');
+var _ = require('lodash');
+
+const SELECTOR_CONTENT = cut(css.content);
+const SELECTOR_VISIBLE = cut(css.visible).substring(1);
 
 class Splash {
   @extend({ css })
   render () {
     return this.$context.getWatcherData();
+  }
+
+  bind () {
+    document
+      .querySelector(SELECTOR_CONTENT)
+      .classList.add(SELECTOR_VISIBLE);
   }
 }
 
